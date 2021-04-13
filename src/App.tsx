579a36a77aa8373c3f3581f4b498567b3daa25f4
RenanJPaula <renanjohannsen@gmail.com>
2021-04-13 19:59:09 -0300

    Adição da store de authenticacao com redux persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import ApplicationRoutes from './routes/application-routes';
-import store from './store/store-config';
+import { store, persistor } from './store/store-config';
 
 const App: React.FC = () => {
   return (
     <ReduxProvider store={store}>
-      <Router>
-        <ApplicationRoutes />
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <ApplicationRoutes />
+        </Router>
+      </PersistGate>
     </ReduxProvider>
   );
 }
--- src/store/store-config.ts
+++ src/store/store-config.ts
@@ -1,8 +1,22 @@
 import { applyMiddleware, createStore, Store } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import { AppState } from './app-state';
 import reducers from './reducers-config';
 
-const store: Store<AppState> = createStore(reducers, applyMiddleware(thunk));
+const storesToPersist: string[] = [
+    'authState'
+];
 
-export default store;
+const persistedReducer = persistReducer({
+    key: 'appState',
+    storage,
+    whitelist: storesToPersist
+}, reducers);
+
+const store: Store<AppState> = createStore(persistedReducer, applyMiddleware(thunk));
+
+const persistor = persistStore(store);
+
+export { store, persistor };
